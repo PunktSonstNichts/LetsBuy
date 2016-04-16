c0f5fc5222fe483e56f2785cdfb7f7f3aab639a9
PunktSonstNichts <t.meyer-arlt@ostfalia.de>
2016-04-17 00:20:18 +0200

    first login test

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 var app = {
-	BASE_URL: "127.0.0.1", //Because I will always love you
+	BASE_URL: "http://139.59.164.70", //Because I will always love you
+	lat: 0.0,
+	long: 0.0,
 	init: function(){
 		// starting site
-		$("#content").html(app.template("home_page"));
-
+		// $("#content").html(app.template("home_page"));
+		$("#content").html(app.template("login"));
+		// Geo stuff
+		app.geo();
 		// Check for Login
+		app.ajax("user", {auth: "PunktSonstNichts", password: "12345"});
 
 		// For the main 3 areas
 		$(document).on("click touchstart",".navigation_item[data-pagelink][data-pagelink!='']",function() {
@@ -30,8 +35,16 @@ var app = {
 			// follow link
 			$("#content").html(app.template($(this).attr("data-templatelink")));
 		});
-	}, 
+	},
 	template: function(name){
+		switch(name){
+			case "login":
+			case "regristation":
+				$("#app").addClass("hiddenHeading");
+			break;
+			default:
+				$("#app").removeClass("hiddenHeading");
+		}
 		html = $("*[data-template='" + name + "']").wrap('<p/>').parent().html();
 		$("*[data-template='" + name + "']").unwrap('<p/>');
 		return html;
@@ -41,28 +54,56 @@ var app = {
 
 		switch(service){
 			case "user":
-				url = app.BASE_URL + "user.php";
+				url = app.BASE_URL + "/user";
+				type = 'POST';
 				break;
 			case "shopping_list":
 				url = app.BASE_URL + "user.php";
+				type = 'POST';
 				break;
 			default:
 				// ERROR CLASS
 		}
+		$.ajaxSetup({
+			beforeSend: function(xhr) {
+		        xhr.withCredentials = true;
+		        xhr.setRequestHeader('Accept', 'application/json');
+		    }
+		});
 		$.ajax({
-			url: 'http://api.joind.in/v2.1/talks/10889',
-			data: {
-				format: 'json'
+			url: url,
+			data: data,
+			type: type,
+			error: function(data) {
+	            if( data.status === 422 ) {
+		            //process validation errors here.
+		            var errors = data.responseJSON; 
+
+		            console.log(errors);
+	            } else {
+
+	            }
 			},
-			error: function() {
-				// ERROR CLASS
-			},
-			dataType: 'jsonp',
 			success: function(data) {
 				// PARSE DATA AND THEN RETURN
-			},
-			type: 'GET'
+			}
 		});
+	},
+	geo: function(){
+		if (navigator.geolocation) {
+			(function rungeo() {
+			    navigator.geolocation.getCurrentPosition(updatePosition);
+
+			    setTimeout(rungeo, 9000);
+			})();
+	    } else {
+	    	// ERROR CLASS
+	    }
+
+	    function updatePosition(positon){
+	    	app.lat = positon.coords.latitude;
+	    	app.long = positon.coords.longitude;
+	    }
 	}
 
 };
