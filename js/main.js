8459a6324c9731c1a74b4ecfa15efe45a735fcba
PunktSonstNichts <t.meyer-arlt@ostfalia.de>
2016-04-16 22:06:06 +0200

    working on adding shopping lists

--- js/main.js
+++ js/main.js
@@ -1,18 +1,40 @@
 var app = {
 	BASE_URL: "127.0.0.1", //Because I will always love you
 	init: function(){
+		// starting site
+		$("#content").html(app.template("home_page"));
+
 		// Check for Login
 
-		$(document).on("click","*[data-templatelink][data-templatelink!='']",function() {
-
-			$("*[data-templatelink][data-templatelink!='']").removeClass("active");
+		// For the main 3 areas
+		$(document).on("click touchstart",".navigation_item[data-pagelink][data-pagelink!='']",function() {
+			 // remove back button
+			$(".navigation_item[data-pagelink][data-pagelink!='']").removeClass("active");
 			$(this).addClass("active");
-			$("#content").html(app.template($(this).attr("data-templatelink")));
+			$("#back_btn").hide();
+			$("#content").html(app.template($(this).attr("data-pagelink")));
 
+		});
+		// For the Back btn
+		$("#back_btn").hide();
+		$(document).on("click touchstart","#back_btn",function() {
+			// append back button
+			$("#back_btn").hide();
+			// follow link
+			$("#content").html(app.template($(".navigation_item.active[data-pagelink][data-pagelink!='']").attr("data-pagelink")));
+		});
+		// For the rest
+		$(document).on("click touchstart","*[data-templatelink][data-templatelink!='']",function() {
+			// append back button
+			$("#back_btn").show();
+			// follow link
+			$("#content").html(app.template($(this).attr("data-templatelink")));
 		});
 	}, 
 	template: function(name){
-		return $("*[data-template='" + name + "']").html();
+		html = $("*[data-template='" + name + "']").wrap('<p/>').parent().html();
+		$("*[data-template='" + name + "']").unwrap('<p/>');
+		return html;
 	},
 	ajax: function(service, data){
 		// CHECK FOR CONNECTION HERE #TODO
